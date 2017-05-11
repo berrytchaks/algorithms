28928969639b4cfb450d925f6d594ab5362c964a
raymond <raymond.wouapi@wouri.co>
2017-05-11 22:30:31 +0100

    min cost to reach the last cell

--- src/dp/MinCost.java
+++ src/dp/MinCost.java
@@ -4,7 +4,7 @@ package dp;
  * 
  * @author berrytchaks
  * 
- *         Minimum Cost problem
+ * Minimum Cost problem
  */
 public class MinCost {
 
@@ -15,7 +15,12 @@ public class MinCost {
 			return (y < z) ? y : z;
 		}
 	}
-
+	
+	private int min2(int x, int y) {
+		return (x < y) ? x : y;
+	}
+	
+	// from the first cell to a given cell
 	// This a naive solution which compute subproblems many times
 	private int minCost(int[][] cost, int m, int n) {
 		if (m < 0 || n < 0) {
@@ -32,7 +37,7 @@ public class MinCost {
 	private int minCostOptimal(int[][] cost, int m, int n) {
 		int[][] tc = new int[cost.length][cost[0].length];
 
-		// initialize the if case of tc
+		// initialize the first cell of tc
 		tc[0][0] = cost[0][0];
 
 		// Compute the minimum cost the first line of tc
@@ -51,6 +56,34 @@ public class MinCost {
 		}
 		return tc[m][n];
 	}
+	
+	// From a given cell to the bottom cell, given that you can only move right and down
+	private int minCostOptimal2(int[][] cost, int m, int n) {
+		int row = cost.length, col = cost[0].length;
+		int[][] tc = new int[row][col];
+		
+		// For conviennient let decrement both row and col
+		row--;
+		col--;
+		// initialize the last cell of tc
+		tc[row][col] = cost[row][col];
+
+		// Compute the minimum cost the last line of tc
+		for (int i = col-1; i >0; i--) {
+			tc[row][i] = tc[row][i+1] + cost[row][i];
+		}
+		// Compute the minimum cost  the last column of tc
+		for (int i = row-1; i >=1; i--) {
+			tc[i][col] = tc[i+1][col] + cost[i][col];
+		}
+		// Compute the minimum cost of the rest of the matrix
+		for (int i = col-1; i >=1; i--) {
+			for (int j = row-1; j >=1; j--) {
+				tc[i][j] = min2(tc[i+1][j],tc[i][j+1]) + cost[i][j];
+			}
+		}
+		return tc[m][n];
+	}
 
 	public static void main(String[] args) {
 		int[][] cost = { { 1, 2, 3 }, 
@@ -60,6 +93,7 @@ public class MinCost {
 		int min = minCost.minCost(cost, 2, 1);
 		System.out.println("Minimal cost with naive approach "+min);
 		System.out.println("Minimal cost with optimal approach "+minCost.minCostOptimal(cost, 2, 1));
+		System.out.println("Minimal cost with optimal approach to reach the last cell "+minCost.minCostOptimal2(cost, 1, 1));
 	}
 
 }
